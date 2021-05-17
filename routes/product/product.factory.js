b9a0aeb19063c7e7ce04e5cc696f2567846976ff
unknown <Aghazjunaid>
2021-05-17 13:24:27 +0530

    add product api

--- routes/product/product.factory.js
+++ routes/product/product.factory.js
@@ -52,6 +52,22 @@ try {
 res.json(return_response);
 }
 
+//=============add product===========================================
+async function addProduct(req,res){
+    var return_response = { "status": null, "message": null, "data": {} } 
+    try {
+        let opt = req.body;
+        const product = new Product(opt);
+        const doc = await product.save();
+        return_response.status = 200;
+        return_response.message = "Product added successfully";
+        return_response.data = doc;
+    } catch (error) {
+        return_response.status = 400;
+        return_response.message = String(error);
+    }
+    res.json(return_response);
+}
 
 
 
@@ -63,7 +79,8 @@ res.json(return_response);
 return {
     addCategory,
     getCategory,
-    deleteCcategory
+    deleteCcategory,
+    addProduct
 }
 
 }
